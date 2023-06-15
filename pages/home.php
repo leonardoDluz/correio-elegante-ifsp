d3ce8081381319426ae0f7978eb5d3ebecf4a2bc
leonardoDluz <thestuff.luz@gmail.com>
2023-06-14 21:47:32 -0300

    remoção da página inicial

--- pages/home.php
+++ pages/home.php
@@ -1,14 +1,15 @@
 <?php require_once __DIR__ . '/../pages/beginning-html.php'; ?>
-
 <main>
-    <h2 class="title">Bem-Vindo!</h2>
+    <h1 class="title">Bem Vindo ao Correio Elegante!</h1>
     
-    <p class="text">O Correio Elegante IFSP é um site onde você pode enviar uma mensagem para algum aluno do Instituo Federal do Campus Itapetininga anonimamente.</p>
+    <h2 class="secondary-title">Envie Sua Mensagem Anônima</h2>
     
-    <a class="button" href="/menu">Envie sua Mensagem</a>
-
-    <a class="button" href="https://api.whatsapp.com/send?phone=5515998382760&text=ola,%20gostaria%20de%20comprar%20um%20n%C3%BAmero%20da%20rifa">Compre Nossa Rifa do Dia dos Namorados</a>
-
+    <a class="button" href="/mensagem?tipo=simples">Apenas a Mensagem R$3,00</a>
+    
+    <a class="button" href="/mensagem?tipo=com-pirulito">A Mensagem + Pirulito R$3,50</a>
+    
+    <a class="button" href="/mensagem?tipo=com-fini">A Mensagem + Fini R$4,00</a>
 </main>
 
 <?php require_once __DIR__ . '/../pages/end-html.php';
+
--- pages/mensages-center.php
+++ pages/mensages-center.php
@@ -1,42 +1,44 @@
 <?php require_once __DIR__ . '/../pages/beginning-html.php';?>
 <main>
     <form method="post">
-        <table>
-            <tr>
-            <th>id</th>
-            <th>nome</th>
-            <th>curso</th>
-            <th>série</th>
-            <th>conteudo</th>
-            <th>tipo</th>
-            <th>data</th>
-            <th>hora</th>
-            <th>status do pagamento</th>
-        </tr>
-
-        <?php foreach ($mensageList as $mensage) { ?>
-            
-        <tr class="data-row">
-            <td class="id"><?=$mensage['id'];?></td>
-            <td><?=$mensage['name'];?></td>
-            <td><?=$mensage['course'];?></td>
-            <td><?=$mensage['grade'];?></td>
-            <td><?=$mensage['content'];?></td>
-            <td><?=$mensage['type'];?></td>
-            <td><?=$mensage['date'];?></td>
-            <td><?=$mensage['hours'];?></td>
-            <td class="<?=$mensage['payment_status'] === 'pago' ? 'pago':'';?>"><?=$mensage['payment_status'];?></td>
-
-            <?php if ($mensage['payment_status'] === 'pendente') { ?>
-                
-            <td class="payment-status"><input type="checkbox" name="payment:<?=$mensage['id'];?>" value="<?= $mensage['id'];?>"></td>
-
-            <?php } ?>
-        </tr>
-
-        <?php } ?>
-        </table>
-        <button type="submit" class="form-button">Confirmar Pagamento</button>
+        <button type="submit" class="form-button mensage-center-button">Confirmar Pagamento</button>
+        <div class="table">
+            <table>
+                    <tr>
+                    <th>id</th>
+                    <th>nome</th>
+                    <th>curso</th>
+                    <th>série</th>
+                    <th>conteudo</th>
+                    <th>tipo</th>
+                    <th>data</th>
+                    <th>hora</th>
+                    <th>status do pagamento</th>
+                </tr>
+    
+                <?php foreach ($mensageList as $mensage) { ?>
+                    
+                <tr class="data-row">
+                    <td class="id"><?=$mensage['id'];?></td>
+                    <td><?=$mensage['name'];?></td>
+                    <td><?=$mensage['course'];?></td>
+                    <td><?=$mensage['grade'];?></td>
+                    <td><?=$mensage['content'];?></td>
+                    <td><?=$mensage['type'];?></td>
+                    <td><?=$mensage['date'];?></td>
+                    <td><?=$mensage['hours'];?></td>
+                    <td class="<?=$mensage['payment_status'] === 'pago' ? 'pago':'';?>"><?=$mensage['payment_status'];?></td>
+    
+                    <?php if ($mensage['payment_status'] === 'pendente') { ?>
+                        
+                    <td class="payment-status"><input type="checkbox" name="payment:<?=$mensage['id'];?>" value="<?= $mensage['id'];?>"></td>
+    
+                    <?php } ?>
+                </tr>
+    
+                <?php } ?>
+            </table>
+        </div>
     </form>
 </main>
 <?php require_once __DIR__ . '/../pages/end-html.php';
